1375989612f840c2899051c447fe2dbbfc6e6f12
Kipngetich Josphat <joskipngetich07@gmail.com>
2025-05-06 15:11:04 +0300

    normalization 1NF and 2NF

--- answers.sql
+++ answers.sql
@@ -1,36 +1,49 @@
--- Question 1 --
--- Split ProductDetail table into two tables i.e products table and productsDetail table to achieve 1NF
--- Creating products table
-CREATE TABLE products(
-    productID INT AUTO_INCREMENT PRIMARY KEY,
-    productName VARCHAR(100)
-);
-
--- Creating productDetail table
--- productID foreign key is used to establish relationship between two tables ensuring referential integrity
-CREATE TABLE productDetails(
-    productDetailsID INT AUTO_INCREMENT PRIMARY KEY,
-    OrderID INT ,
-    customerName VARCHAR(100),
-    productID INT,
-    FOREIGN KEY(productID) REFERENCES products(productID)
-);
-
--- Question 2 ---
--- Moving partially dependent attributes to their own table i.e customer name attribute and product to achieve 2NF
--- Creating customers table
-CREATE TABLE customers(
-   customerID INT AUTO_INCREMENT PRIMARY KEY,
-   customerName VARCHAR(100)
-);
-
---Creating orderDetails table with two foreign keys to link customers table and products table with order details table
-CREATE TABLE orderDetails(
-    orderDetailsID INT AUTO_INCREMENT PRIMARY KEY,
+-- Question 1
+-- Create ProductDetail table to achieve 1NF
+CREATE TABLE ProductDetail(
     OrderID INT,
-    customerID INT,
-    productID INT,
-    quantity INT,
-    FOREIGN KEY(customerID) REFERENCES customers(customerID),
-    FOREIGN KEY(productID) REFERENCES products(productID)
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
 );
+
+--Insert sample data into ProductDetail table
+INSERT INTO ProductDetail(OrderID,CustomerName,Products)
+VALUES
+(101,'John Doe','Laptop'),
+(101,'John Doe','Mouse'),
+(102,'Jane Smith','Tablet'),
+(102,'Jane Smith','Keyboard'),
+(102,'Jane Smith','Mouse'),
+(103,'Emily Clark','Phone');
+
+-- Create Orders table
+CREATE TABLE Orders(
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Insert INTO Orders table
+INSERT INTO Orders(OrderID,CustomerName)
+VALUES
+(101,'John Doe'),
+(102,'Jane Smith'),
+(103,'Emily Clark');
+
+-- Create Product table
+CREATE TABLE Product(
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY(OrderID,Product),
+    FOREIGN KEY(OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert into Product table
+INSERT INTO Product(OrderID,Product,Quantity)
+VALUES
+(101,'Laptop',2),
+(101,'Mouse',1),
+(102,'Tablet',3),
+(102,'Keyboard',1),
+(102,'Mouse',2),
+(103,'Phone',1);
